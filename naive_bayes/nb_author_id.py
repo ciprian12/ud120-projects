debd244484e2144db11cc3237044f52e1e91648f
Ciprian Lucaci <Ciprian.Lucaci@ness.com>
2017-01-18 11:04:27 +0200

    support machine vector solution

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,6 +30,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 clf = GaussianNB()
 
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
@@ -38,7 +39,9 @@ t1 = time()
 pred = clf.predict(features_test)
 print "prediction time:", round(time()-t1, 3), "s"
 
+t2 = time()
 accuracy =  clf.score(features_test, labels_test)
+print "accuracy time:", round(time()-t2, 3), "s"
 
 print accuracy
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,6 +10,7 @@
     
 import sys
 from time import time
+from sklearn import svm
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -20,10 +21,54 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+
+
+# clf = svm.SVC(kernel='linear')
+# clf = svm.SVC(kernel='rbf', C=10.0)
+# clf = svm.SVC(kernel='rbf', C=100.0)
+# clf = svm.SVC(kernel='rbf', C=1000.0)
+clf = svm.SVC(kernel='rbf', C=10000.0)
+
+
+# optional: reduce training set
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy =  clf.score(features_test, labels_test)
+
+print accuracy
+
+nth = 10
+print "element %d : %d" % (nth, pred[nth])
+
+nth = 26
+print "element %d : %d" % (nth, pred[nth])
+
+
+nth = 50
+print "element %d : %d" % (nth, pred[nth])
+
+
+nth = 100
+print "element %d : %d" % (nth, pred[nth])
+
+
+count_chris = 0
+for i in xrange(0,len(pred)):
+    if pred[i] == 1:
+        count_chris = count_chris + 1
+
+print "chris emails %d" % count_chris
 
 #########################################################
 
