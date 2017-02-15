82b103329e041149f37e851f5103a4dcbad666ab
Ciprian Lucaci <Ciprian.Lucaci@ness.com>
2017-02-15 13:38:19 +0200

    pca lesson complete

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -18,6 +18,9 @@ The dataset used in this example is a preprocessed excerpt of the
 
 print __doc__
 
+# import Image
+# from PIL import Image
+
 from time import time
 import logging
 import pylab as pl
@@ -67,6 +70,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
 n_components = 150
+# n_components = 250
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -81,6 +85,12 @@ X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
 
+for variance in pca.explained_variance_ratio_:
+    print "variance-ratio: %f" % variance
+
+
+# for variance in pca.explained_variance_:
+#     print "variance: %d" % variance
 
 ###############################################################################
 # Train a SVM classification model
