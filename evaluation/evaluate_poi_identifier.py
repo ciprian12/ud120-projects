f783ff5fce4b70d8bfd69efba60047edf3668200
Ciprian Lucaci <Ciprian.Lucaci@ness.com>
2017-02-17 16:11:22 +0200

    evaluation code complete

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -29,3 +29,68 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+from sklearn import tree
+from time import time
+
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split( features, labels, test_size=0.3, random_state=42)
+
+# clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = tree.DecisionTreeClassifier()
+
+# features_train = features
+# features_test = features
+
+# labels_train = labels
+# labels_test = labels
+
+print "test feature_test_size %d" % len(features_test)
+print "test feature_train_size %d" % len(features_train)
+
+print "test labels_test_size %d" % len(labels_test)
+print "test labels_train_size %d" % len(labels_train)
+
+
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+accuracy = clf.score(features_train, labels_train)
+print "accuracy of %s" % accuracy
+
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print "predicted size %d" % len(pred)
+
+accuracy = clf.score(features_test, labels_test)
+print "accuracy of %s" % accuracy
+
+poi_count = 0
+for p in pred:
+    if p == 1.0:
+        poi_count += 1
+
+print "poi_count: %d" % poi_count
+
+not_equal_count = 0
+for i in xrange(0, len(pred)):
+    print "actual %s pred %s" % (labels_test[i], pred[i])
+    if labels_test[i] != pred[i]:
+        not_equal_count +=1
+
+print "not_equal_count %d" % not_equal_count
+
+
+# from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+precision = precision_score(labels_test, pred)
+recall = recall_score(labels_test, pred)
+
+print "precision: %s" % precision
+print "recall: %s" % recall
